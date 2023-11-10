dcec1ea3c47912a2bd262a6acf10eb6c9f4f0953
Kathleen Mercado <kathleenmerc@gmail.com>
2023-11-10 14:56:47 -0800

    feat: add modal section with styling and event listeners for the submit and close buttons

--- main.js
+++ main.js
@@ -1,10 +1,47 @@
-/*  CTA button events  */
+/* ----- CTA Button Events ----- */
 const ctaButton = document.getElementById("cta-button");
 const contactSection = document.getElementById("contact-section");
+
 ctaButton.addEventListener("click", scrollToContact);
 
 function scrollToContact() {
   contactSection.scrollIntoView({
     behavior: "smooth",
   });
+}
+
+/* ----- Submit Button Events ----- */
+const submitButton = document.getElementById("submit-button");
+const modal = document.getElementById("modal-section");
+const overlay = document.getElementById("overlay");
+const requiredFormFieldsInput = document.querySelectorAll(".required input");
+
+submitButton.addEventListener("click", openModal);
+
+function openModal(event) {
+  let isFormValid = true;
+
+  // Check each input value for a string and trim to remove whitespace
+  requiredFormFieldsInput.forEach((input) => {
+    if (!input.value.trim()) {
+      isFormValid = false;
+    }
+  });
+
+  // Open modal if the required form fields are filled
+  if (isFormValid) {
+    event.preventDefault();
+    modal.classList.remove("hidden");
+    overlay.classList.remove("hidden");
+  }
+}
+
+/* ----- Close Button Events ----- */
+const closeButton = document.getElementById("close-button");
+
+closeButton.addEventListener("click", closeModal);
+
+function closeModal() {
+  modal.classList.add("hidden");
+  overlay.classList.add("hidden");
 }
